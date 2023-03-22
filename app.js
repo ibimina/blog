d4b35746e3100e00a7cd8eae9c1520d24f29797c
Ibimina hart <ibimina.c.hart@gmail.com>
2023-03-22 21:51:19 +0100

    docs: render a array of blogs

--- app.js
+++ app.js
@@ -9,7 +9,21 @@ app.listen(process.env.PORT || 3000);
 
 //basic routing
 app.get('/',(req,res)=>{
-    res.render('index')
+    const blogs =[{
+        title: 'Ibimina starts a blog',
+        snippet: 'Lorem ipsum dolor sit amet consectetur'
+    },
+    {
+        title: 'Node js is the best',
+        snippet: 'Lorem ipsum dolor sit amet consectetur'
+    },
+    {
+        title: 'How to defeat bowser',
+        snippet: 'Lorem ipsum dolor sit amet consectetur'
+    }
+
+]
+    res.render('index',{title: 'Home', blogs});
 })
 
 app.get("/about", (req, res) => {
