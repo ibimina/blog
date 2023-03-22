a16f7c17ce5a01f2242904148ecf14fe3bea3deb
Ibimina hart <ibimina.c.hart@gmail.com>
2023-03-22 15:58:06 +0100

    docs: edit port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app =express()
 app.set('view engine', 'ejs');
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000);
 //basic routing
 app.get('/',(req,res)=>{
     res.render('index')
