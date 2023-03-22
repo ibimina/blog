b77eeda1e86af0caddaede77a61f4a51fdca8e94
Ibimina hart <ibimina.c.hart@gmail.com>
2023-03-22 17:18:31 +0100

    docs: add static file, about and 404 route

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 
 const app =express()
+app.use(express.static("public"));
+
 app.set('view engine', 'ejs');
 
 app.listen(process.env.PORT || 3000);
+
 //basic routing
 app.get('/',(req,res)=>{
     res.render('index')
 })
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.use( (req, res) => {
+  res.render("404");
+});
